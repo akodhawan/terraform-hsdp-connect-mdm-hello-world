560ab734cdfd4e478f57061f77c6e7979932d76f
Akhil <akhil.dhawan@philips.com>
2021-11-25 17:51:33 +0100

    Updated MDM TH & IAM Pro/App/Service

--- MDM/mdm-th.tf
+++ MDM/mdm-th.tf
@@ -1,9 +1,17 @@
 module "connect_onboarding" {
   source = "philips-labs/connect-onboarding/hsdp"
-
   onboarding_iam_org_id = hsdp_iam_org.example_org.id
-  provisioning_service_id = hsdp_iam_service.deserve_service.id
-  admin_users = [for user in hsdp_iam_user.institute_user: user.id]
-  self_service_users = [for user in hsdp_iam_user.institute_user: user.id]
+  provisioning_service_id = hsdp_iam_service.example_service.id
+  admin_users = [for user in hsdp_iam_user.example_user: user.id]
+  self_service_users = [for user in hsdp_iam_user.example_user: user.id]
+}
+
+
+resource "hsdp_connect_mdm_proposition" "testprop1" {
+  name        = "FUNNYPRODUCT"
+  description = "Terraform managed FUNNYPRODUCT proposition"
+  organization_id = hsdp_iam_org.example_org.id
+  status              = "ACTIVE"
+  depends_on          = [module.connect_onboarding] 
 }
 
