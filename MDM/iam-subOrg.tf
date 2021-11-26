a5b1f832b96d175d0dc45cdad7436e50234ce07c
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-11-26 15:17:48 +0100

    Add randomness

--- MDM/iam-subOrg.tf
+++ MDM/iam-subOrg.tf
@@ -1,11 +1,11 @@
 resource "hsdp_iam_org" "example_org" {
-  name          = "EXAMPLE_SUB_ORG_TF"
+  name            = "TF-IAM-${random_pet.deploy.id}"
   description   = "Example Sub Organization"
   parent_org_id = var.example_root_org
 }
 
 resource "hsdp_iam_role" "example_role" {
-  name = "EXAMPLE_ROLE_TF"
+  name            = "TF-IAM-${random_pet.deploy.id}"
   permissions = [
     "HSDP_IAM_ORGANIZATION.READ",
     "USER.READ"
@@ -14,8 +14,8 @@ resource "hsdp_iam_role" "example_role" {
 }
 
 resource "hsdp_iam_user" "example_user" {
-  for_each = {for staff in var.example_users:  staff.email_id => staff}
-  
+  for_each = { for staff in var.example_users : staff.email_id => staff }
+
   login           = each.value.email_id
   email           = each.value.email_id
   first_name      = each.value.first_name
@@ -24,31 +24,31 @@ resource "hsdp_iam_user" "example_user" {
 }
 
 resource "hsdp_iam_group" "example_group" {
-  name                  = "EXAMPLE_GROUP_TF"
+  name            = "TF-${random_pet.deploy.id}"
   roles                 = [hsdp_iam_role.example_role.id]
-  users                 = concat([var.org_admin_username],[for user in hsdp_iam_user.example_user: user.id])
+  users                 = concat([var.org_admin_username], [for user in hsdp_iam_user.example_user : user.id])
   managing_organization = hsdp_iam_org.example_org.id
 }
 
 resource "hsdp_iam_proposition" "example_prop" {
-  name                = "EXAMPLEPROPOSITION"
-  description         = "Eaxmple Proposition"
-  organization_id     = hsdp_iam_org.example_org.id
+  name            = "TF-IAM-${random_pet.deploy.id}"
+  description     = "Eaxmple Proposition"
+  organization_id = hsdp_iam_org.example_org.id
 }
 
 resource "hsdp_iam_application" "example_app" {
-  name                = "EXAMPLEAPP"
-  description         = "Example application"
-  proposition_id      = hsdp_iam_proposition.example_prop.id
+  name            = "TF-IAM-${random_pet.deploy.id}"
+  description    = "Example application"
+  proposition_id = hsdp_iam_proposition.example_prop.id
 }
 
 resource "hsdp_iam_service" "example_service" {
-  name                = "EXAMPLESERVICE"
-  description         = "Example service"
-  application_id      = hsdp_iam_application.example_app.id
+  name            = "TF-IAM-${random_pet.deploy.id}"
+  description    = "Example service"
+  application_id = hsdp_iam_application.example_app.id
 
-  validity            = 12
+  validity = 12
 
-  scopes              = ["openid"]
-  default_scopes      = ["openid"]
+  scopes         = ["openid"]
+  default_scopes = ["openid"]
 }
--- MDM/main.tf
+++ MDM/main.tf
@@ -1,9 +1,58 @@
-provider "hsdp" {
-  region             = var.region
-  environment        = var.environment
-  oauth2_client_id   = var.oauth2_client_id
-  oauth2_password    = var.oauth2_password
-  org_admin_username = var.org_admin_username
-  org_admin_password = var.org_admin_password
-  # debug_log = "./debug.log"
+resource "random_pet" "deploy" {
+}
+
+module "connect_onboarding" {
+  source                  = "philips-labs/connect-onboarding/hsdp"
+  onboarding_iam_org_id   = hsdp_iam_org.example_org.id
+  provisioning_service_id = hsdp_iam_service.example_service.id
+  admin_users             = concat([var.org_admin_username], [for user in hsdp_iam_user.example_user : user.id])
+  self_service_users      = concat([var.org_admin_username], [for user in hsdp_iam_user.example_user : user.id])
+}
+
+
+resource "hsdp_connect_mdm_proposition" "testprop1" {
+  name            = "TF-MDM-${random_pet.deploy.id}"
+  description     = "Terraform managed ${random_pet.deploy.id} proposition"
+  organization_id = hsdp_iam_org.example_org.id
+  status          = "ACTIVE"
+  depends_on      = [module.connect_onboarding]
+}
+
+resource "hsdp_connect_mdm_application" "testapp1" {
+  name           = "TF-MDM-${random_pet.deploy.id}"
+  proposition_id = hsdp_connect_mdm_proposition.testprop1.id
+}
+
+resource "hsdp_connect_mdm_device_group" "testgroup1" {
+  name           = "TF-MDM-${random_pet.deploy.id}"
+  description    = "A funny-product device group"
+  application_id = hsdp_connect_mdm_application.testapp1.id
+}
+
+resource "hsdp_connect_mdm_device_type" "testdevicetype1" {
+  name                   = "TF-MDM-${random_pet.deploy.id}"
+  description            = "WEARABLE0001"
+  commercial_type_number = "WATCH1"
+  device_group_id        = hsdp_connect_mdm_device_group.testgroup1.id
+  custom_type_attributes = {
+    position = "wrist"
+    region   = "eu"
+  }
+}
+
+resource "hsdp_connect_mdm_oauth_client" "testclient1" {
+  name                = "TF-MDM-${random_pet.deploy.id}"
+  description         = "Test client"
+  application_id      = hsdp_connect_mdm_application.testapp1.id
+  global_reference_id = "c105f8c2-d62c-4d9f-8810-50adbd883ca8"
+
+  scopes         = []
+  default_scopes = []
+
+  redirection_uris = [
+  ]
+
+  response_types = ["code", "code id_token"]
+
+  user_client = true
 }
